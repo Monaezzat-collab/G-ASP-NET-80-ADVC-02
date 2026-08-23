7647adbd5cf6f258cf9580198c707687c1e5ebdd
Monaezzat-collab <monaezzat664@gmail.com>
2026-08-23 22:00:04 +0300

    Task 01 : Smart Product Search

---
+++ OrderProcessing/Product.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderProcessing
+{
+    public class Product
+    {
+    
+        //public Product(int id, string? name, string? category, double price, int stock)
+        //{
+        //    Id = id;
+        //    Name = name;
+        //    Category = category;
+        //    Price = price;
+        //    Stock = stock;
+        //}
+
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Category { get; set; }
+        public double Price { get; set; }
+        public int Stock { get; set; }
+
+        public static List<Product> SearchProducts(List<Product> products, Func<Product, bool> condition)
+            
+        {
+            List<Product> result = new List<Product>();
+
+            foreach (Product product in products) {
+                if (condition(product)) {
+                    result.Add(product);
+                }  
+            }
+            return result;
+        }
+
+    }
+}
--- OrderProcessing/Program.cs
+++ OrderProcessing/Program.cs
@@ -4,7 +4,79 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //We need this system to be flexible, making the system easy to extend without modification. >> Use Delegate
+            #region Starter Code: Data Models & Product Catalog 
+            //Product class and product catalog
+
+            List<Product> catalog = new()
+            {
+                new Product {Id=1,Name="Laptop",Category="Electronics",Price=1200,Stock=10},
+
+                new Product {Id=2,Name="Phone",Category="Electronics",Price=800,Stock=25},
+
+                new Product {Id=3,Name="T-Shirt",Category="Clothing",Price=30,Stock=100},
+
+                new Product {Id=4,Name="Jeans",Category="Clothing",Price=60,Stock=50},
+
+                new Product {Id=5,Name="Chocolate",Category="Food",Price=5,Stock=200},
+
+                new Product {Id=6, Name="Coffee Beans",Category="Food",Price=15,Stock=80},
+
+                new Product{Id=7, Name="C# Book",Category="Books",Price=45,Stock=30},
+
+                new Product{Id=8, Name="Novel",Category="Books",Price=20,Stock=60},
+
+                new Product{Id=9, Name="Headphones",Category="Electronics",Price=150,Stock=40},
+
+                new Product{Id=10, Name="Jacket",Category="Clothing",Price=120,Stock=15},
+
+
+
+
+
+            };
+
+            #endregion
+
+            #region Task 01 : Smart Product Search
+            
+            List<Product> electronics=Product.SearchProducts(catalog, item=>item.Category=="Electronics");
+            List<Product> cheap = Product.SearchProducts(catalog, item => item.Price < 50);
+            List<Product> inStock = Product.SearchProducts(catalog, item => item.Stock > 0);
+            List<Product> cheapClothing = Product.SearchProducts(catalog, item => item.Category == "Clothing" && item.Price < 100);
+
+            //1. All Electronics products 
+
+            Console.WriteLine("----- Electronics  ----- ");
+            foreach(Product product in electronics)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price} -[Stock: {product.Stock}]");
+            }
+            //2. Products cheaper than $50 
+
+            Console.WriteLine("----- Under $50 -----");
+            foreach (Product product in cheap)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price} -[Stock: {product.Stock}]");
+
+            }
+
+            //3. Products that are in stock (Stock > 0) 
+            Console.WriteLine("----- In Stock -----");
+            foreach( Product product in inStock)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price} -[Stock: {product.Stock}]");
+
+            }
+
+            //4. Clothing products under $100
+            Console.WriteLine("------- Clothing Under $100 -------");
+            foreach(Product product in cheapClothing)
+            {
+                Console.WriteLine($"{product.Name} - ${product.Price} -[Stock: {product.Stock}]");
+
+            }
+            #endregion
         }
     }
 }
